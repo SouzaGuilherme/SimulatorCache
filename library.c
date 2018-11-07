9cc639afd1d85d556c52005be131aeb7b6522a0d
TeilorFerrari <ti.ferrari@inf.ufpel.edu.br>
2018-11-07 02:36:43 -0200

    function conversion, binary for decimal

--- library.c
+++ library.c
@@ -1,1 +1,22 @@
 // Library.c the functions of library.h the simulator Cache
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+int main()
+{
+    char bin[100];
+    unsigned long dec = 0;
+    int i = 0;
+    int s;
+    fgets( bin, sizeof(bin), stdin);
+    s = strlen( bin );
+    while( s-- ) {
+        if( bin[s] == '0' || bin[s] == '1' ) {
+            dec = dec + pow(2, i++) * (bin[s] - '0');
+        }
+    };
+    printf("\nDecimal Equivalent of Binary Number: \t %u\n", dec);
+    return 0;
+}
