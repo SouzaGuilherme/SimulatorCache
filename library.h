c2026f7d5fc446b783315419723bcd620dbf7dcc
TeilorFerrari <ti.ferrari@inf.ufpel.edu.br>
2018-11-07 01:17:38 -0200

    add set Struct cache

--- library.h
+++ library.h
@@ -1,1 +1,13 @@
 // Library.h of simulator cache
+#ifndef cache
+#define cache
+
+#include <stdint.h> 
+struct status{
+	unsigned long hits_l1;
+	unsigned long misses_l1
+};
+
+struct status *cache(int tamanho_bloco, int tamanho_cache, char *filename);
+
+#endif
